acf9554568b36d6c15eaec3bbb1f66314fad5190
Tom Dunand <84025296+Tom-D04@users.noreply.github.com>
2026-01-18 20:37:27 +0100

    Création d'un dataset pour l'évaluation du modèle sur des musiques personnalisées.

---
+++ project/src/eval_dataset.py
@@ -0,0 +1,71 @@
+SAMPLES_DIR = "project/audio/evaluation_samples"
+
+# Dataset de test
+DATASET = [
+    # ELECTRONIC
+    {"file": "HOUSE_move_your_body.mp3", "genre": "electronic", 
+     "subgenres": ["House music", "Electronic music", "Electronica"], 
+     "instruments": ["Synthesizer", "Drum machine"]},
+    {"file": "TECHNO_LaLaLand.mp3", "genre": "electronic", 
+     "subgenres": ["Techno", "Electronic music"], 
+     "instruments": ["Synthesizer", "Drum machine"]},
+    {"file": "DUBSTEP_scary_monsters_and_nice_spirites.mp3", "genre": "electronic", 
+     "subgenres": ["Dubstep", "Electronic music"], 
+     "instruments": ["Synthesizer"]},
+    
+    # ROCK 
+    {"file": "ROCK_Iron_Man.mp3", "genre": "rock", 
+     "subgenres": ["Rock music", "Rock and roll"], 
+     "instruments": ["Electric guitar", "Drum kit"]},
+    {"file": "METAL_Duality.mp3", "genre": "rock", 
+     "subgenres": ["Heavy metal", "Rock music"], 
+     "instruments": ["Electric guitar", "Drum kit"]},
+    
+    # POP
+    {"file": "POP_castle_on_the_hill_ed-sheeran.mp3", "genre": "pop", 
+     "subgenres": ["Pop music"], 
+     "instruments": ["Singing", "Synthesizer"]},
+    
+    # HIP-HOP
+    {"file": "RAP_can-t_stop_me_Tupac.mp3", "genre": "hip-hop", 
+     "subgenres": ["Hip hop music", "Rapping"], 
+     "instruments": ["Rapping", "Drum machine"]},
+    {"file": "TRAP_March_Madness.mp3", "genre": "hip-hop", 
+     "subgenres": ["Hip hop music", "Trap music"], 
+     "instruments": ["Rapping", "Synthesizer"]},
+    
+    # JAZZ
+    {"file": "JAZZ_the_girl_from_ipanema.mp3", "genre": "jazz", 
+     "subgenres": ["Jazz"], 
+     "instruments": ["Saxophone", "Piano", "Drum kit"]},
+    
+    # CLASSICAL
+    {"file": "CLASSICAL_DVORÁK.mp3", "genre": "classical", 
+     "subgenres": ["Classical music", "Orchestra"], 
+     "instruments": ["Violin, fiddle", "String section"]},
+    
+    # SOUL/FUNK
+    {"file": "SOUL-FUNK_seven_minutes_of_funk.mp3", "genre": "soul/funk", 
+     "subgenres": ["Funk", "Disco"], 
+     "instruments": ["Bass guitar", "Drum kit"]},
+
+    # REGGAE
+    {"file": "REGGAE_night_nurse.mp3", "genre": "reggae", 
+     "subgenres": ["Reggae"], 
+     "instruments": ["Electric guitar", "Bass guitar"]},
+
+    # BLUES
+    {"file": "BLUES_Love_Bug_Blues.mp3", "genre": "blues", 
+     "subgenres": ["Blues"], 
+     "instruments": ["Electric guitar", "Harmonica"]},
+
+    # LATIN
+    {"file": "LATIN_Manu_Chao_Me_Gustas_Tu.mp3", "genre": "latin", 
+     "subgenres": ["Salsa music", "Music of Latin America"], 
+     "instruments": ["Percussion", "Trumpet"]},
+
+    # AFROBEAT
+    {"file": "AFROBEAT_waist.mp3", "genre": "world", 
+     "subgenres": ["Afrobeat", "Music of Africa"], 
+     "instruments": ["Percussion", "Electric guitar"]},
+]
